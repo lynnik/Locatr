213ef6d1e90f5161ea00741bbc34124eaf3c8643
Lynnik Vlad <lynnik.v@gmail.com>
2017-08-01 11:53:29 +0300

    Added receiving og geo-position data.

--- app/src/main/java/com/lynnik/locatr/LocatrFragment.java
+++ app/src/main/java/com/lynnik/locatr/LocatrFragment.java
@@ -1,8 +1,12 @@
 package com.lynnik.locatr;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,10 +16,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
 public class LocatrFragment extends Fragment {
+
+  private static final String TAG = "LocatrFragment";
 
   private ImageView mImageView;
   private GoogleApiClient mClient;
@@ -78,5 +86,40 @@ public class LocatrFragment extends Fragment {
 
     MenuItem searchItem = menu.findItem(R.id.action_locate);
     searchItem.setEnabled(mClient.isConnected());
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_locate:
+        findImage();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  private void findImage() {
+    LocationRequest request = LocationRequest.create();
+    request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    request.setNumUpdates(1);
+    request.setInterval(0);
+
+    if (ActivityCompat.checkSelfPermission(
+        getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(
+        getActivity(),
+        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      return;
+    }
+    LocationServices.FusedLocationApi
+        .requestLocationUpdates(mClient, request, new LocationListener() {
+          @Override
+          public void onLocationChanged(Location location) {
+            Log.i(TAG, "Got a fix: " + location);
+          }
+        });
   }
 }
